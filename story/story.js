291b005f3982312d8140017ac218e02383f5f34c
Irenheard <146376146+Irenheard@users.noreply.github.com>
2024-01-25 16:56:26 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -4,51 +4,51 @@ https://developers.google.com/maps/documentation/javascript/examples/polyline-si
 */
 
 function initMap() {
-	// location variables???
+	// location variables
+	// toronto pearson airport
 	const toronto = { lat: 43.677, lng: -79.632 };
+	//london heathrow airport
+	const london = { lat: 51.468, lng: -0.454 };
+	// joburg tambo airport... i got detained here lol
+	const joburg = { lat: -26.136, lng: 28.241 };
+	// Pick'n'Pay... neat little grocery/liquor store
+	const mokopane = { lat: -24.187, lng: 29.012 };
+	// Pidwa wilderness reserve!!!
+	const askari = { lat: -24.064, lng: 30.559 };
 	
 	const map = new google.maps.Map(document.getElementById("map"), {
 	zoom: 3,
 	center: { lat: 15.608, lng: -19.072 },
-	mapTypeId: "terrain",
+	mapTypeId: "satellite",
 	});
  
     const torontoToLondon = [
-	// Toronto Pearson Airport
-    //{ lat: 43.677, lng: -79.632 },
-	toronto,
-	// London Heathrow Airport
-    { lat: 51.468, lng: -0.454 },
+    toronto,
+    london,
   ];
     const flightOne = new google.maps.Polyline({
     path: torontoToLondon,
     geodesic: true,
-    strokeColor: "#000000",
+    strokeColor: "#00ccff",
     strokeOpacity: 1.0,
     strokeWeight: 2,
   });
   
     const londonToJoburg = [
-	// London Heathrow Airport
-    { lat: 51.468, lng: -0.454 },
-	// Joburg Tambo Airport
-	// i got detained here lol... it was my fault
-    { lat: -26.136, lng: 28.241 },
+    london,
+    joburg,
   ];
     const flightTwo = new google.maps.Polyline({
     path: londonToJoburg,
     geodesic: true,
-    strokeColor: "#000000",
+    strokeColor: "#00ccff",
     strokeOpacity: 1.0,
     strokeWeight: 2,
   });
   
     const joburgToMokopane = [
-	// Joburg Tambo Airport
-    { lat: -26.136, lng: 28.241 },
-	// Pick'n'Pay
-    { lat: -24.187, lng: 29.012 },
-
+    joburg,
+    mokopane,
   ];
     const driveOne = new google.maps.Polyline({
     path: joburgToMokopane,
@@ -59,11 +59,8 @@ function initMap() {
   });
   
     const mokopaneToAskari = [
-	// Pick'n'Pay
-    { lat: -24.187, lng: 29.012 },
-	// Pidwa Wilderness Reserve
-    { lat: -24.064, lng: 30.559 },
-
+    mokopane,
+    askari,
   ];
     const driveTwo = new google.maps.Polyline({
     path: mokopaneToAskari,
@@ -73,30 +70,10 @@ function initMap() {
     strokeWeight: 2,
   });
 
-  flightOne.setMap(map)
-  flightTwo.setMap(map)
-  driveOne.setMap(map)
-  driveTwo.setMap(map)
-  
-   
-
-/*
-  const flightPlanCoordinates = [
-    { lat: 37.772, lng: -122.214 },
-    { lat: 21.291, lng: -157.821 },
-    { lat: -18.142, lng: 178.431 },
-    { lat: -27.467, lng: 153.027 },
-  ];
-  const flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: "#FF0000",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
-  
-  flightPath.setMap(map);
-*/
+	flightOne.setMap(map)
+	flightTwo.setMap(map)
+	driveOne.setMap(map)
+	driveTwo.setMap(map)
 }
 
 window.initMap = initMap;
