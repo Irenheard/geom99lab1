e816ba4a3b72c85ebfadf2a32558c4ff80792e52
Irenheard <146376146+Irenheard@users.noreply.github.com>
2024-01-25 17:38:13 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,10 +1,11 @@
+function initMap() {
+	
 /*
 template referenced:
 https://developers.google.com/maps/documentation/javascript/examples/polyline-simple#maps_polyline_simple-html 
 */
 
-function initMap() {
-	// location variables
+	/* location variables*/
 	// toronto pearson airport
 	const toronto = { lat: 43.677, lng: -79.632 };
 	//london heathrow airport
@@ -17,63 +18,99 @@ function initMap() {
 	const askari = { lat: -24.064, lng: 30.559 };
 	
 	const map = new google.maps.Map(document.getElementById("map"), {
-	zoom: 3,
-	center: { lat: 15.608, lng: -19.072 },
-	mapTypeId: "satellite",
+		zoom: 3,
+		center: { lat: 15.608, lng: -19.072 },
+		mapTypeId: "satellite",
 	});
  
     const torontoToLondon = [
-    toronto,
-    london,
-  ];
+		toronto,
+		london,
+	];
     const flightOne = new google.maps.Polyline({
-    path: torontoToLondon,
-    geodesic: true,
-    strokeColor: "#00ccff",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
+		path: torontoToLondon,
+		geodesic: true,
+		strokeColor: "#00ccff",
+		strokeOpacity: 1.0,
+		strokeWeight: 2,
+	});
   
     const londonToJoburg = [
-    london,
-    joburg,
-  ];
+		london,
+		joburg,
+	];
     const flightTwo = new google.maps.Polyline({
-    path: londonToJoburg,
-    geodesic: true,
-    strokeColor: "#00ccff",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
+		path: londonToJoburg,
+		geodesic: true,
+		strokeColor: "#00ccff",
+		strokeOpacity: 1.0,
+		strokeWeight: 2,
+	});
   
     const joburgToMokopane = [
-    joburg,
-    mokopane,
-  ];
+		joburg,
+		mokopane,
+	];
     const driveOne = new google.maps.Polyline({
-    path: joburgToMokopane,
-    geodesic: true,
-    strokeColor: "#ce7e00",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
+		path: joburgToMokopane,
+		geodesic: true,
+		strokeColor: "#ff00ff",
+		strokeOpacity: 1.0,
+		strokeWeight: 2,
+	});
   
     const mokopaneToAskari = [
-    mokopane,
-    askari,
-  ];
+		mokopane,
+		askari,
+	];
     const driveTwo = new google.maps.Polyline({
-    path: mokopaneToAskari,
-    geodesic: true,
-    strokeColor: "#ce7e00",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
+		path: mokopaneToAskari,
+		geodesic: true,
+		strokeColor: "##ff00ff",
+		strokeOpacity: 1.0,
+		strokeWeight: 2,
+	});
 
 	flightOne.setMap(map)
 	flightTwo.setMap(map)
 	driveOne.setMap(map)
 	driveTwo.setMap(map)
-}
+	
+/*
+template for the legend referenced:
+https://developers.google.com/maps/documentation/javascript/adding-a-legend#maps_legend-javascript
+*/
+	const icons = {
+		flight: {
+			name: "Drive",
+			icon: "drive.png",
+		},
+		smcircle: {
+			name: "Flight",
+			icon:"flight.png",
+		},
+	};
+	
+	const legend = document.getElementById("legend");
+
+	for (const key in icons) {
+		const type = icons[key];
+		const name = type.name;
+		const icon = type.icon;
+		const div = document.createElement("div");
+
+		div.innerHTML = '<img src="' + icon + '"> ' + name;
+		legend.appendChild(div);
+	}
+
+	map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
+	
+	
+	
+	
+	
+}// last bracket
+
+
 
 window.initMap = initMap;
